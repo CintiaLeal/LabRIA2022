04064a8daa4c8078e020b7d383da9287c6fad857
leandro3742 <leandro.marrero@outlook.com>
2022-06-19 18:10:13 -0300

    arreglos en clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,29 @@ import { AppComponent } from './app.component';
 import { NoticiasComponent } from './Page/Noticias/noticias.component';
 import { LoginComponent } from './Page/Login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { RegistrarComponent } from './Page/Registrar/registrar.component';
+import { MaterialExampleModule } from 'src/material.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
-  declarations: [
-    AppComponent,
-    NoticiasComponent,
-    LoginComponent,
-    RegistrarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NoticiasComponent,
+        LoginComponent,
+        RegistrarComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MaterialExampleModule,
+        MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
