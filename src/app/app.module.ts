bd2cf7449ee77c6eb50dec80670abdc18909dc35
leandro3742 <leandro.marrero@outlook.com>
2022-06-22 14:34:53 -0300

    add navbar responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { RegistrarComponent } from './Page/Registrar/registrar.component';
-import { MaterialExampleModule } from 'src/material.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
 @NgModule({
     declarations: [
         AppComponent,
@@ -20,13 +21,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         RegistrarComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
-        MaterialExampleModule,
         MatCardModule,
+        MatIconModule,
         MatInputModule,
         MatButtonModule,
         MatToolbarModule,
+        MatMenuModule
+
     ],
     providers: [],
     bootstrap: [AppComponent]
