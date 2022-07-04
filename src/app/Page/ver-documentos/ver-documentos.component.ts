905db20651763f2b5ff3ad5e3cdf180a60f26ee9
CintiaLeal <cintialeal31m@gmail.com>
2022-07-04 13:03:47 -0300

    No me anda el btn de descargar pdf en verdocumento de interes

--- src/app/Page/ver-documentos/ver-documentos.component.ts
+++ src/app/Page/ver-documentos/ver-documentos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { DocumentosService } from 'src/app/servicios/documentos.service';
+import { Documento } from 'src/app/modelos/documentos';
+import { Observable, subscribeOn, Subscriber } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-ver-documentos',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-documentos.component.css']
 })
 export class VerDocumentosComponent implements OnInit {
-
-  constructor() { }
+  public documentosIF:Documento[] =[];
+  public documentosOL:Documento[] =[];
+  public documentosDI:Documento[] =[];
+  public base64Image: any;
+  panelOpenState = false;
+  constructor(private _formBuilder: FormBuilder,private service:DocumentosService,private alerta: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.service.getDocumentoIC().subscribe({
+      next: value => this.documentosIF = value,
+      error: err => { alert('Error al cargar los documentos: ' + err) }
+    });
+    this.service.getDocumentoOL().subscribe({
+      next: value => this.documentosOL = value,
+      error: err => { alert('Error al cargar los documentos: ' + err) }
+    });
+    this.service.getDocumentoDI().subscribe({
+      next: value => this.documentosDI = value,
+      error: err => { alert('Error al cargar los documentos: ' + err) }
+    });
+  }
+  
+  convertToBase64(file: File) {
+    console.log(file);
+    const observable = new Observable((subscriber: Subscriber<any>) => {
+      this.readFile(file, subscriber);
+    })
+
+    observable.subscribe((d) => {
+      this.base64Image = d;
+    })
   }
 
+  readFile(file: File, subscriber: Subscriber<any>) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file)
+
+    fileReader.onload = () => {
+      subscriber.next(fileReader.result)
+      subscriber.complete()
+    }
+    fileReader.onerror = () => {
+      subscriber.error()
+    }
+  }
+  
+  onFileSelected(event: any): void {
+     this.convertToBase64(event.target.files[0]);
+   }
+  
 }
